690b7d4aee1f8629f81868a01e9b3f4dafbb7938
Evgeny Zasetsky <zasetsky.evgeny@gmail.com>
2022-10-10 19:07:21 +0300

    Move idea

--- src/components/Elevator.vue
+++ src/components/Elevator.vue
@@ -1,13 +1,32 @@
 <template>
-    <div>
+    <div class="caption-text" :style="{marginTop:y + 'px'}" @click="click()">
         
     </div>
 </template>
 <script>
 export default {
-    name: 'ElevatorComponent'
+    name: 'ElevatorComponent',
+    data() {
+    return {
+      y: 500
+    }
+  },
+    methods: {
+    click() {
+      this.y -=99
+      if(this.y < 0) {
+        this.y += 500
+      }
+    }
+  },
 }
 </script>
-<style lang="">
-    
+<style lang="scss">
+.caption-text {
+    width: 100px;
+    height: 100px;
+    margin-left: 50px;
+    background-color: red;
+    transition: all 3s;
+  }
 </style>
