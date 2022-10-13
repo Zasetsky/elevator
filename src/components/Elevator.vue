8898bdf5885ccb040bdc54ba4fef82cb7dc3bd0a
Evgeny Zasetsky <zasetsky.evgeny@gmail.com>
2022-10-13 17:15:59 +0300

    floorsCount

--- src/components/Elevator.vue
+++ src/components/Elevator.vue
@@ -6,7 +6,8 @@
       :style="{marginTop: position + 'px', transition: speed + 's'}">
     </div>
     <Floor 
-    v-for="(item, id) in items" :key="id"
+    v-for="(item, id) in floors" :key="id"
+    :floorsButton="floors"
     :id="id"
     @buttonClick="move"
     />
@@ -17,14 +18,16 @@ import Floor from './Floor.vue'
 
 export default {
     name: "ElevatorComponent",
+    components: { Floor },
     data() {
         return {
             isActiveElevator: false,
-            items: 5,
+            floorsCount: 5,
             isOpenDoors: false,
             position: 500,
             height: 125,
-            oldPosition: 0,
+            oldPosition: 500,
+            floors: []
         };
     },
     computed: {
@@ -32,33 +35,43 @@ export default {
             return Math.abs(this.position - this.oldPosition) / this.height;
         },
     },
+    mounted() {
+      for(let i = 0; i < this.floorsCount; i++) {
+        this.floors.push({
+          id: i,
+          isActive: false
+        })
+      }
+    },
     methods: {
         move(id) {
             this.oldPosition = this.position;
             if (!this.isActiveElevator && !this.isOpenDoors) {
                 this.isActiveElevator = true;
+                
                 this.position = id * this.height;
                 setTimeout(this.deactivatingElevator, this.speed * 1000);
                 setTimeout(this.openDoors, this.speed * 1000);
             }
         },
+
         deactivatingElevator() {
             this.isActiveElevator = false;
         },
+
         openDoors() {
             this.isOpenDoors = true;
             setTimeout(this.closeDoors, 3000);
         },
+
         closeDoors() {
             this.isOpenDoors = false;
         }
-    },
-    components: { Floor }
+    }
 }
 </script>
 <style lang="scss">
 .shaft {
-  position: relative;
 
   &__elevator {
     position: absolute;
